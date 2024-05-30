2f32be500a24f1b572352d3d84d7d04ba05fe66a
shilpatumma <shilpatumma21@gmail.com>
2024-05-30 13:23:41 +0530

    stopwatch added

--- tic-tac-toe-game.js
+++ tic-tac-toe-game.js
@@ -1,5 +1,5 @@
 // Function called whenever user tab on any box 
-function myfunc() {
+function game() {
 
     // Setting DOM to all boxes or input field 
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
@@ -299,16 +299,10 @@ function myfunc() {
     }
 }
 
-// Function to reset game 
-function myfunc_2() {
-    location.reload();
-    b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = '';
-}
 
-// Here onwards, functions check turn of the player 
-// and put accordingly value X or 0 
+
 flag = 1;
-function myfunc_3() {
+function game3() {
     if (flag == 1) {
         document.getElementById("b1").value = "X";
         document.getElementById("b1").disabled = true;
@@ -321,8 +315,7 @@ function myfunc_3() {
     }
 }
 
-
-function myfunc_4() {
+function game4() {
     if (flag == 1) {
         document.getElementById("b2").value = "X";
         document.getElementById("b2").disabled = true;
@@ -335,7 +328,7 @@ function myfunc_4() {
     }
 }
 
-function myfunc_5() {
+function game5() {
     if (flag == 1) {
         document.getElementById("b3").value = "X";
         document.getElementById("b3").disabled = true;
@@ -348,7 +341,7 @@ function myfunc_5() {
     }
 }
 
-function myfunc_6() {
+function game6() {
     if (flag == 1) {
         document.getElementById("b4").value = "X";
         document.getElementById("b4").disabled = true;
@@ -361,7 +354,7 @@ function myfunc_6() {
     }
 }
 
-function myfunc_7() {
+function game7() {
     if (flag == 1) {
         document.getElementById("b5").value = "X";
         document.getElementById("b5").disabled = true;
@@ -374,7 +367,7 @@ function myfunc_7() {
     }
 }
 
-function myfunc_8() {
+function game8() {
     if (flag == 1) {
         document.getElementById("b6").value = "X";
         document.getElementById("b6").disabled = true;
@@ -387,7 +380,7 @@ function myfunc_8() {
     }
 }
 
-function myfunc_9() {
+function game9() {
     if (flag == 1) {
         document.getElementById("b7").value = "X";
         document.getElementById("b7").disabled = true;
@@ -400,7 +393,7 @@ function myfunc_9() {
     }
 }
 
-function myfunc_10() {
+function game10() {
     if (flag == 1) {
         document.getElementById("b8").value = "X";
         document.getElementById("b8").disabled = true;
@@ -413,7 +406,7 @@ function myfunc_10() {
     }
 }
 
-function myfunc_11() {
+function game11() {
     if (flag == 1) {
         document.getElementById("b9").value = "X";
         document.getElementById("b9").disabled = true;
