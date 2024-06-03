014fabaa753daf44c7b931c31ad405e3a9a07e07
shilpatumma <shilpatumma21@gmail.com>
2024-06-03 13:25:39 +0530

    4 task completed

--- game.js
+++ game.js
@@ -303,118 +303,104 @@ function game() {
 
 flag = 1;
 function game1() {
-    if (flag == 1) {
-        document.getElementById("b1").value = "X";
-        document.getElementById("b1").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b1").value = "0";
-        document.getElementById("b1").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b1").value = "X";
+    document.getElementById("b1").disabled = true;
+    randomGenerator();
 }
 
 function game2() {
-    if (flag == 1) {
-        document.getElementById("b2").value = "X";
-        document.getElementById("b2").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b2").value = "0";
-        document.getElementById("b2").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b2").value = "X";
+    document.getElementById("b2").disabled = true;
+    randomGenerator();
 }
 
 function game3() {
-    if (flag == 1) {
-        document.getElementById("b3").value = "X";
-        document.getElementById("b3").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b3").value = "0";
-        document.getElementById("b3").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b3").value = "X";
+    document.getElementById("b3").disabled = true;
+    randomGenerator();
 }
 
 function game4() {
-    if (flag == 1) {
-        document.getElementById("b4").value = "X";
-        document.getElementById("b4").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b4").value = "0";
-        document.getElementById("b4").disabled = true;
-        flag = 1;
-    }
+
+    document.getElementById("b4").value = "X";
+    document.getElementById("b4").disabled = true;
+    randomGenerator();
 }
 
 function game5() {
-    if (flag == 1) {
-        document.getElementById("b5").value = "X";
-        document.getElementById("b5").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b5").value = "0";
-        document.getElementById("b5").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b5").value = "X";
+    document.getElementById("b5").disabled = true;
+    randomGenerator();
 }
 
 function game6() {
-    if (flag == 1) {
-        document.getElementById("b6").value = "X";
-        document.getElementById("b6").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b6").value = "0";
-        document.getElementById("b6").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b6").value = "X";
+    document.getElementById("b6").disabled = true;
+    randomGenerator();
 }
 
 function game7() {
-    if (flag == 1) {
-        document.getElementById("b7").value = "X";
-        document.getElementById("b7").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b7").value = "0";
-        document.getElementById("b7").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b7").value = "X";
+    document.getElementById("b7").disabled = true;
+    randomGenerator();
 }
 
 function game8() {
-    if (flag == 1) {
-        document.getElementById("b8").value = "X";
-        document.getElementById("b8").disabled = true;
-        flag = 0;
-    }
-    else {
-        document.getElementById("b8").value = "0";
-        document.getElementById("b8").disabled = true;
-        flag = 1;
-    }
+    document.getElementById("b8").value = "X";
+    document.getElementById("b8").disabled = true;
+    randomGenerator();
 }
 
 function game9() {
-    if (flag == 1) {
-        document.getElementById("b9").value = "X";
-        document.getElementById("b9").disabled = true;
-        flag = 0;
+    document.getElementById("b9").value = "X";
+    document.getElementById("b9").disabled = true;
+    randomGenerator();
+}
+function randomGenerator()
+{
+    x = Math.floor(Math.random() * (9 - 1) + 1);
+    rdmfunction(x);
+}
+
+function rdmfunction(x) {    
+   
+    if (x == 1 && document.getElementById("b1").disabled == false) {
+        document.getElementById("b1").value = "0";
+        document.getElementById("b1").disabled = true;
     }
-    else {
+    else if (x == 2 && document.getElementById("b2").disabled == false) {
+        document.getElementById("b2").value = "0";
+        document.getElementById("b2").disabled = true;
+    }
+    else if (x == 3 && document.getElementById("b3").disabled == false) {
+        document.getElementById("b3").value = "0";
+        document.getElementById("b3").disabled = true;
+    }
+    else if (x == 4 && document.getElementById("b4").disabled == false) {
+        document.getElementById("b4").value = "0";
+        document.getElementById("b4").disabled = true;
+    }
+    else if (x == 5 && document.getElementById("b5").disabled == false) {
+        document.getElementById("b5").value = "0";
+        document.getElementById("b5").disabled = true;
+    }
+    else if (x == 6 && document.getElementById("b6").disabled == false) {
+        document.getElementById("b6").value = "0";
+        document.getElementById("b6").disabled = true;
+    }
+    else if (x == 7 && document.getElementById("b7").disabled == false) {
+        document.getElementById("b7").value = "0";
+        document.getElementById("b7").disabled = true;
+    }
+    else if (x == 8 && document.getElementById("b8").disabled == false) {
+        document.getElementById("b8").value = "0";
+        document.getElementById("b8").disabled = true;
+    }
+    else if (x == 9 && document.getElementById("b9").disabled == false) {
         document.getElementById("b9").value = "0";
         document.getElementById("b9").disabled = true;
-        flag = 1;
     }
-} 
+    else {
+        randomGenerator();
+    }
+}
